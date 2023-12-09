61e497a9ab5857075ef800e352bbf4ca2c7a2dd2
RafaelJoelson <144927205+RafaelJoelson@users.noreply.github.com>
2023-12-09 01:32:30 -0300

    Adicionando estrutura HTML/CSS ao projeto e incluindo vídeo
    - Criadas páginas HTML para apresentação do perfil, formação, projetos e formulário de contato.
    - Estilização CSS aplicada para melhorar o layout e a aparência das páginas.
    - Inserido um vídeo em uma das seções.

--- projeto_site_rafael/app_website/views.py
+++ projeto_site_rafael/app_website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
+def education(request):
+    return render(request,'education.html')
+def portfolio(request):
+    return render(request,'portfolio.html')
+def contact(request):
+    return render(request,'contact.html')
--- projeto_site_rafael/projeto_site_rafael/urls.py
+++ projeto_site_rafael/projeto_site_rafael/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('education/', views.education, name='education'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
 ]
+
